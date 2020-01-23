65f3ac0530b1f7e28010b72bbea45c20be52f640
Jorge Sanz <jorgesanz883@gmail.com>
2020-01-23 13:02:26 +0100

    viernes

--- src/Contador/ContadorTest.java
+++ src/Contador/ContadorTest.java
@@ -9,10 +9,20 @@ class ContadorTest {
 	@Test
 	void testContador1() 
 	{
+		int resultado;
 		int x=1;
 		int y=9;
 		Contador ct1 = new Contador();
-		int resultado = Contador.contador1(x, y);
+		resultado = ct1.contador1(x, y);
 	}
-
+	@Test
+	void testContador2() 
+	{
+		int esperado=-1;
+		int x=-1;
+		int y=9;
+		Contador ct2 = new Contador();
+		int resultado = ct2.contador1(x, y);
+		assertEquals(esperado, resultado);
+	}
 }
